5f000633fdd069ea5f6e296065e2a8645c0fe0bd
dylbobin <dylansilva22@yahoo.com>
2024-03-07 10:13:35 -0800

    Initial Commit

---
+++ Task-It/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task-It
+//
+//  Created by Dylan Silva on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task-It/Task_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ItApp.swift
+//  Task-It
+//
+//  Created by Dylan Silva on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
